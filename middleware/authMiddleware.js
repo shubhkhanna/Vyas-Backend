91710583b44833c32feefde9761c8afcdcc2da05
shubhkhanna <khannashubh2001@gmail.com>
2022-04-10 15:26:27 +0530

    ft: admin middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 
 const authMiddleware = asyncHandler(async (req, res, next) => {
   let token;
@@ -15,7 +16,7 @@ const authMiddleware = asyncHandler(async (req, res, next) => {
       //   decoding the token
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      req.user = decoded;
+      req.user = await User.findById(decoded.userId).select("-password");
 
       next();
     } catch (error) {
@@ -40,4 +41,14 @@ const authMiddleware = asyncHandler(async (req, res, next) => {
   }
 });
 
-module.exports = authMiddleware;
+// Admin Middleware
+const adminMiddleware = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Unauthorized Access, Not an Admin!");
+  }
+};
+
+module.exports = { authMiddleware, adminMiddleware };
